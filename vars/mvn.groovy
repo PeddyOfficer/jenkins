74c7dedcb76a491519d02cdfb133f7cc01e53015
roger <roger.pedersen@tieto.com>
2018-11-08 22:02:50 +0100

    Try compose run

--- vars/mvn.groovy
+++ vars/mvn.groovy
@@ -5,7 +5,9 @@ def call(env, mavenCommand) {
     final String name = "'$composeId'_mvn_1"
     //sh "CURRENT_UID=`id -u` CURRENT_GID=`id -g` MVN_CMD='$mavenCommand' docker-compose -f docker-compose.yml --no-ansi -p $composeId up mvn"
     final String cmd = 'echo $?'
-    final int exitCode = sh(returnStdout: true, script: "CURRENT_UID=`id -u` CURRENT_GID=`id -g` MVN_CMD='$mavenCommand' docker-compose -f docker-compose.yml --no-ansi -p $composeId up mvn; $cmd").trim()
+    final String docker = "CURRENT_UID=`id -u` CURRENT_GID=`id -g` MVN_CMD='$mavenCommand' docker-compose -f docker-compose.yml --no-ansi -p $composeId up mvn; $cmd"
+    final String exitCode = sh(returnStdout: true, script: "$docker").trim()
+    
     //final int exitCode = sh(returnStdout: true, script: "docker inspect $name --format={{.State.ExitCode}}").trim()
 
     echo "EXIT = $exitCode"
