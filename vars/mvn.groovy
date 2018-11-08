b511c0a4e9cffd18dc8847f2c9fd0538c89024c8
roger <roger.pedersen@tieto.com>
2018-11-08 21:03:56 +0100

    Try compose run

--- vars/mvn.groovy
+++ vars/mvn.groovy
@@ -4,7 +4,9 @@ def call(env, mavenCommand) {
     final String composeId = uniqueComposeId(env)
     final String name = "'$composeId'_mvn_1"
     sh "CURRENT_UID=`id -u` CURRENT_GID=`id -g` MVN_CMD='$mavenCommand' docker-compose -f docker-compose.yml --no-ansi -p $composeId up mvn"
-    final int exitCode = sh(returnStdout: true, script: "docker inspect $name --format={{.State.ExitCode}}").trim()
+    final String cmd = 'echo $?'
+    final int exitCode = sh(returnStdout: true, script: "docker run --rm mvn mvn -B -Duser.home=/tmp -s /build/settings.xml '$mavenCommand'; '$cmd'").trim()
+    //final int exitCode = sh(returnStdout: true, script: "docker inspect $name --format={{.State.ExitCode}}").trim()
 
     if(exitCode != 0) {
         throw new Exception("Maven build failed")
