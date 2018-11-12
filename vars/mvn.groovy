a22d7d938f3acd1d42560a340a595ef6e57409f6
roger <roger.pedersen@tieto.com>
2018-11-12 14:05:48 +0100

    Fix exception

--- vars/mvn.groovy
+++ vars/mvn.groovy
@@ -6,7 +6,7 @@ def call(env, mavenCommand) {
     final String docker = "CURRENT_UID=`id -u` CURRENT_GID=`id -g` MVN_CMD='$mavenCommand' docker-compose -f docker-compose.yml --no-ansi -p $composeId up --exit-code-from mvn mvn"
     final String exitCode = sh(returnStdout: true, script: "$docker").trim()
 
-    if(exitCode.equalsIgnoreCase("0")) {
+    if(!exitCode.equalsIgnoreCase("0")) {
         throw new Exception("Maven build failed")
     }
 }
