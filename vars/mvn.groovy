efd6df7c52c7df0daba03640ddc7c0035cb0a565
roger <roger.pedersen@tieto.com>
2018-11-12 14:27:58 +0100

    No need to fetch exit code - just fail

--- vars/mvn.groovy
+++ vars/mvn.groovy
@@ -3,10 +3,5 @@
 def call(env, mavenCommand) {
     final String composeId = uniqueComposeId(env)
 
-    final String docker = "CURRENT_UID=`id -u` CURRENT_GID=`id -g` MVN_CMD='$mavenCommand' docker-compose -f docker-compose.yml --no-ansi -p $composeId up --exit-code-from mvn mvn"
-    final String exitCode = sh(returnStdout: true, script: "$docker").trim()
-
-    //if(!exitCode.equalsIgnoreCase("0")) {
-    //    throw new Exception("Maven build failed")
-    //}
+    sh "CURRENT_UID=`id -u` CURRENT_GID=`id -g` MVN_CMD='$mavenCommand' docker-compose -f docker-compose.yml --no-ansi -p $composeId up --exit-code-from mvn mvn"
 }
